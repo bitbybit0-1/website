8938a3b6a0e0a9132f3cdb1a4622c986a9bd1658
joseph sutera <joesutera11@gmail.com>
2022-05-04 12:32:48 -0400

    website phase 0

---
+++ cite.js
@@ -0,0 +1,24 @@
+${};
+const liberty=document.getElementById("click");
+
+liberty.addEventListener('click',function(){
+    
+    liberty.src="flag.jpeg";
+    })
+  
+    let shooter= document.getElementById('color');
+
+    shooter.addEventListener('mouseover', function(event){
+       console.log(event)
+       
+        shooter.style.color="blue";
+
+        const grasshopper= document.getElementById("onclick");
+        grasshopper.addEventListener('click',function(){
+        alert("fugehduhboudit")
+
+        }) 
+            
+        })
+
+
